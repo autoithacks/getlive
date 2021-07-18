3ff2932c2a5283e1802219438be6a03bad956313
autoithacks <87604753+autoithacks@users.noreply.github.com>
2021-07-18 15:16:26 +0200

    your comment here

--- commit.bat
+++ commit.bat
@@ -1,13 +1,13 @@
 rem git add output.m3u8
 
-git pull
-git push origin master
+rem git pull
+rem git push origin master
 git push -f origin main
 git add .
 
-git commit    -m "your comment here"
+rem git commit    -m "your comment here"
 git commit -a -m "your comment here"
-git push origin main
+rem git push origin main
 
 rem git commit
 
