e76fdba36808e05c3f01e1a0c9b26b5ac559fb84
autoithacks <87604753+autoithacks@users.noreply.github.com>
2021-07-18 15:26:21 +0200

    your comment here

--- commit.bat
+++ commit.bat
@@ -1,16 +1,17 @@
 rem git add output.m3u8
-
 rem git pull
 rem git push origin master
-git push -f origin main
-git add .
 
+git rm "input.txt"
+
+
+git push -f origin main
+rem git add .
 rem git commit    -m "your comment here"
 git commit -a -m "your comment here"
+
 rem git push origin main
-
 rem git commit
-
 rem git push
 
 pause
